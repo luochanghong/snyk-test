de9e3c04892ac144fc752c57f06780bdc196aa79
luochanghong <1415346329@qq.com>
2023-06-15 15:10:20 +0800

    jj

---
+++ build.gradle(1).kts
@@ -0,0 +1,55 @@
+/*
+* Copyright 2014-2021 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+*/
+
+plugins {
+    id("org.gradle.kotlin.kotlin-dsl") version "3.2.4"
+    kotlin("plugin.serialization") version "1.7.21"
+}
+
+val buildSnapshotTrain = properties["build_snapshot_train"]?.toString()?.toBoolean() == true
+
+repositories {
+    maven("https://plugins.gradle.org/m2")
+    maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
+
+    if (buildSnapshotTrain) {
+        mavenLocal()
+    }
+}
+
+sourceSets.main {
+}
+
+val ktor_version = "2.3.0"
+
+dependencies {
+    implementation(kotlin("gradle-plugin", "1.8.10"))
+    implementation(kotlin("serialization", "1.8.10"))
+
+    val ktlint_version = libs.versions.ktlint.version.get()
+    implementation("org.jmailen.gradle:kotlinter-gradle:$ktlint_version")
+
+    implementation("io.ktor:ktor-server-default-headers:$ktor_version")
+    implementation("io.ktor:ktor-server-netty:$ktor_version")
+    implementation("io.ktor:ktor-server-cio:$ktor_version")
+    implementation("io.ktor:ktor-server-jetty:$ktor_version")
+    implementation("io.ktor:ktor-server-websockets:$ktor_version")
+    implementation("io.ktor:ktor-server-auth:$ktor_version")
+    implementation("io.ktor:ktor-server-caching-headers:$ktor_version")
+    implementation("io.ktor:ktor-server-conditional-headers:$ktor_version")
+    implementation("io.ktor:ktor-server-compression:$ktor_version")
+    implementation("io.ktor:ktor-server-content-negotiation:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx:$ktor_version")
+    implementation("io.ktor:ktor-network-tls-certificates:$ktor_version")
+
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.logback.classic)
+}
+
+kotlin {
+    jvmToolchain {
+        check(this is JavaToolchainSpec)
+        languageVersion.set(JavaLanguageVersion.of(8))
+    }
+}
